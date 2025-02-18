9fb5e8dac56b3ce5a5f7bf9051b7a328877e4426
maryunnie <mariannygoncalvesmoreira@gmail.com>
2025-02-18 17:05:13 -0300

    up

---
+++ jsfabrica/src/app/mari_Registro-aparelho/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./aparelho.module.css";
+import Image from "next/image";
+
+export default function aparelho(){
+    return(  
+        <div></div>
+    )
+}
