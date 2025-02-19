ec14cf78dffd9fa63636b36304fbbd44780cbbdb
maryunnie <mariannygoncalvesmoreira@gmail.com>
2025-02-19 18:51:26 -0300

    mary 19/02

--- jsfabrica/src/app/mari_telaplanos/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./planos.module.css";
-import Image from "next/image";
-
-export default function planos(){
-    return(  
-        <div></div>
-    )
-}
