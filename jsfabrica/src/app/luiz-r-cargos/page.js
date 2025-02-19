d0b75a1fec2994ed182341b3a0692b5d30176dbd
luigiliezz <luizantonioatercio@gmail.com>
2025-02-19 11:51:49 -0300

    luiz 19/02

--- jsfabrica/src/app/luiz-r-cargos/page.js
+++ jsfabrica/src/app/luiz-r-cargos/page.js
@@ -1,79 +1,50 @@
-/*
 import React from "react";
-import styles from "./cliente.module.css";
-import Image from "next/image";
+import styles from "./r-cargos.module.css.js"
 
 
+export default function tabela() {
+    const usuarios = [
+        { cargo: "", descricao: "", id: 1 },
+        { cargo: "", descricao: "", id: 2 },
+        { cargo: "", descricao: "", id: 3 },
+        { cargo: "", descricao: "", id: 4 },
+        { cargo: "", descricao: "", id: 5 },
+        { cargo: "", descricao: "", id: 6 },
+        { cargo: "", descricao: "", id: 7 },
+        { cargo: "", descricao: "", id: 8 },
+        { cargo: "", descricao: "", id: 9 }
 
-    <h1> Registro Cargos</h1>
+    ];
 
-    <table>
-        <tr>
-            <th> Cargo </th>
-            <th> Descrição </th>
-            <th> Id </th>
-        </tr>
+    return(
+        <div>
+            <h1> Registro de Cargos</h1>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Cargo</th>
+                        <th>Descrição</th>
+                        <th>Id</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {usuarios.map ((user) => (
+                        <tr key={user.cargo}>
+                            <td> {user.cargo} </td>
+                            <td> {user.descricao} </td>
+                            <td> {user.id} </td>
 
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
+                        </tr>
 
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-        <tr>
-            <td> </td>
-            <td> </td>
-            <td> </td>
-        </tr>
-
-    </table>
-
-    <button id="botao_cargos" type="submit"> Registrar Cargo </button>
-    <button class="botao_secundario" type="submit"> Excluir Cargo </button>
+                    ))}
+                </tbody>
+            </table>
+            <button type="submit" className={styles.button}> Registrar Cargo </button>
+            <button type="submit" className={styles.button}> Excluir Cargo </button>
 
 
+        </div>
 
+    );
+}
 
-export default CadCliente;*/
