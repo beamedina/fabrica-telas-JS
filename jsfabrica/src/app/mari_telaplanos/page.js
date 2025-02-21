6680827b7e75f3f2f92eff79a58dbc44d839ce2e
beamedina <alexandra.medina@estudante.ifro.edu.br>
2025-02-21 13:10:21 -0400

    sla

--- jsfabrica/src/app/mari_telaplanos/page.js
+++ jsfabrica/src/app/mari_telaplanos/page.js
@@ -6,41 +6,41 @@ const Planos = () => {
   return (
     <div>
     <Header/>
-    <section className={styles.plansSection}>
-      <div>
-        <h1 className={styles.h1}>Plano Mensal</h1>
-        <div className={styles.plan}>
-          <p>R$90,00</p>
-          <ul>
-            <li>Acesso a todos os aparelhos</li>
-            <li className={styles.liPlanoMen}>Horários - 05:00 às 23:00</li>
-          </ul>
+    <main className={styles.mainPlanos}>
+      <section className={styles.plansSection}>
+        <div>
+          <h1 className={styles.h1}>Plano Mensal</h1>
+          <div className={styles.plan}>
+            <p>R$90,00</p>
+            <ul>
+              <li>Acesso a todos os aparelhos</li>
+              <li className={styles.liPlanoMen}>Horários - 05:00 às 23:00</li>
+            </ul>
+          </div>
         </div>
-      </div>
-
-      <div>
-        <h2>Plano Anual</h2>
-        <div className={styles.plan}>
-          <p>R$900,00</p>
-          <ul>
-            <li>Acesso a todos os aparelhos</li>
-            <li className={styles.liPlanoAnu}>Horários - 05:00 às 23:00</li>
-          </ul>
+        <div>
+          <h2>Plano Anual</h2>
+          <div className={styles.plan}>
+            <p>R$900,00</p>
+            <ul>
+              <li>Acesso a todos os aparelhos</li>
+              <li className={styles.liPlanoAnu}>Horários - 05:00 às 23:00</li>
+            </ul>
+          </div>
         </div>
-      </div>
-
-      <div>
-        <h3>Plano Vip</h3>
-        <div className={styles.plan}>
-          <p>R$130,00</p>
-          <ul>
-            <li>Acesso a todos os aparelhos</li>
-            <li>Acesso à zumba</li>
-            <li>Acesso à hidroginástica</li>
-          </ul>
+        <div>
+          <h3>Plano Vip</h3>
+          <div className={styles.plan}>
+            <p>R$130,00</p>
+            <ul>
+              <li>Acesso a todos os aparelhos</li>
+              <li>Acesso à zumba</li>
+              <li>Acesso à hidroginástica</li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
     </div>
   );
 };
